26ba8c977f2f341e074d771085e6c34028df3a69
lileiyuhanmeimei <77425212+lileiyuhanmeimei@users.noreply.github.com>
2021-01-18 20:52:37 +0800

    Update gitWord.java

--- src/main/java/gitWord.java
+++ src/main/java/gitWord.java
@@ -2,5 +2,6 @@ public class gitWord {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hot-fox");
+             System.out.println("hot-fox");
     }
 }
